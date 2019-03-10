47ce86756dc146c292be6f8889893a8b86197b67
ghigbie <gehigbie@gmail.com>
2019-03-10 10:53:20 -0600

    Pass var to template

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-app_name = "Chubby Puppy"
+app_name_foundation = "Chubby Puppy"
 
 @app.route('/')
 def index():
-    return render_template('base.html', app_name=app_name)
+    return render_template('base.html', app_name_foundation=app_name_foundation)
 
 
 if __name__ == '__main__':
